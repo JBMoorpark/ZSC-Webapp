1e357438d7457394c66df14a719cffc556f9f921
JBMoorpark <jamesbmitchell@gmail.com>
2022-05-12 19:03:41 -0700

    Added Select dropdowns and Material Type

--- zsc-orders/src/InventoryAdd.tsx
+++ zsc-orders/src/InventoryAdd.tsx
@@ -10,6 +10,7 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { FormHelperText } from '@mui/material';
 
 const materialSources = [
     {
@@ -43,7 +44,6 @@ export const InventoryAdd = () => {
         <Card sx={{ width: 800 }} variant="outlined"><Inventorycard /></Card>
     </Box >
     )
-
 }
 
 
@@ -51,6 +51,11 @@ const Inventorycard = () => {
 
     const [sku, setSku] = React.useState("");
     const [source, setSource] = React.useState("");
+    const [title, setTitle] = React.useState("");
+    const [materialType, setMaterialType] = React.useState("");
+    const [description, setDescription] = React.useState("");
+
+    const [message, setMessage] = React.useState("");
 
     const handleSkuChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSku(e.target.value as string);
@@ -60,12 +65,36 @@ const Inventorycard = () => {
         setSource(e.target.value as string);
     }
 
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value as string);
+    }
+
+    const handleMaterialTypeChange = (e: SelectChangeEvent) => {
+        setMaterialType(e.target.value as string);
+    }
+
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value as string);
+    }
+
+    const onSubmit = () => {
+        const isValid = sku !== "" && source !== "" && title !== "" && materialType !== "";
+        if (isValid) {
+            setMessage("Form submitted successfully");
+
+        } else {
+            setMessage("Please fill required fields.")
+        }
+
+    }
+
+
     return (<React.Fragment>
         <CardContent sx={{ width: 700, }}>
             {/* <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
                 Add New Inventory Item
             </Typography> */}
-            <Grid container spacing={2}>
+            <Grid container spacing={1}>
                 <Grid container item xs={12}>
                     <Grid item xs={2}>
                         SKU
@@ -73,6 +102,8 @@ const Inventorycard = () => {
                     <Grid item xs={10}>
                         {/* <TextField sx={{ width: 250, }} */}
                         <TextField
+                            error={sku === ""}
+                            helperText={sku === "" ? "SKU is required" : ""}
                             required
                             id="sku-required"
                             label="Required"
@@ -85,22 +116,16 @@ const Inventorycard = () => {
                 </Grid>
                 <Grid container item xs={12}>
                     <Grid item xs={2}  >
-                        Material Source
+                        Source
                     </Grid>
                     <Grid item xs={10}>
-                        {/* <TextField
-                            required
-                            id="material-source-required"
-                            label="Required"
-                            variant="outlined"
-                            // size="small"
-                            fullWidth
-                        /> */}
                         <FormControl fullWidth>
                             <InputLabel id="demo-simple-select-label">Source</InputLabel>
                             <Select
+                                error={source === ""}
                                 labelId="demo-simple-select-label"
                                 id="demo-simple-select"
+                                size="small"
                                 value={source}
                                 label="Source"
                                 onChange={handleSourceChange}
@@ -108,6 +133,50 @@ const Inventorycard = () => {
                                 <MenuItem value={'Stock'}>Stock</MenuItem>
                                 <MenuItem value={'Print'}>Print</MenuItem>
                             </Select>
+                            {source === "" && <FormHelperText id="my-helper-text">Select one option from dropdown.</FormHelperText>}
+                        </FormControl>
+                    </Grid>
+                </Grid>
+                <Grid container item xs={12}>
+                    <Grid item xs={2}  >
+                        Source
+                    </Grid>
+                    <Grid item xs={10}>
+                        {/* <TextField
+                            required
+                            id="material-type-required"
+                            label="Required"
+                            variant="outlined"
+                            // size="small"
+                            fullWidth
+                        /> */}
+                        <FormControl fullWidth>
+                            <InputLabel id="material-type-input-label">Mat. Type</InputLabel>
+                            <Select
+                                error={ materialType === ""}
+                                labelId="material-type-select-label"
+                                id="material-type-select"
+                                size="small"
+                                value={materialType}
+                                label="Mat. Type"
+                                onChange={handleMaterialTypeChange}
+                            >
+                                <MenuItem value={'Videos'}>Videos</MenuItem>
+                                <MenuItem value={'Paycheck Stuffers'}>Paycheck Stuffers</MenuItem>
+                                <MenuItem value={'Posting Notices'}>Posting Notices</MenuItem>
+                                <MenuItem value={'Claim Kits'}>Claim Kits</MenuItem>
+                                <MenuItem value={'Posters'}>Posters</MenuItem>
+                                <MenuItem value={'Stickers'}>Stickers</MenuItem>
+                                <MenuItem value={'Other'}>Other</MenuItem>
+                                <MenuItem value={'Folders'}>Folders</MenuItem>
+                                <MenuItem value={'Binders'}>Binders</MenuItem>
+                                <MenuItem value={'Flyers'}>Flyers</MenuItem>
+                                <MenuItem value={'Videos'}>Brochures</MenuItem>
+                                <MenuItem value={'Policy Envelopes'}>Policy Envelopes</MenuItem>
+                                <MenuItem value={'Zenith Paper'}>Zenith Paper</MenuItem>
+                                <MenuItem value={'Covers'}>Covers</MenuItem>
+                            </Select>
+                            {source === "" && <FormHelperText id="my-helper-text">Select one option from dropdown.</FormHelperText>}
                         </FormControl>
                     </Grid>
                 </Grid>
@@ -117,11 +186,15 @@ const Inventorycard = () => {
                     </Grid>
                     <Grid item xs={10}>
                         <TextField sx={{ width: 650 }}
+                            error={title === ""}
+                            helperText={title === "" ? "Must enter title" : ""}
                             required
                             id="title-required"
+                            value={title}
                             label="Required"
                             variant="outlined"
                             size="small"
+                            onChange={handleTitleChange}
                         />
 
                     </Grid>
@@ -133,9 +206,11 @@ const Inventorycard = () => {
                     <Grid item xs={10}>
                         <TextField sx={{ width: 650 }}
                             id="description-optional"
+                            value={description}
                             label="Optional"
                             variant="outlined"
                             size="small"
+                            onChange={handleDescriptionChange}
                         />
                     </Grid>
                 </Grid>
@@ -153,7 +228,14 @@ const Inventorycard = () => {
         </Typography> */}
         </CardContent>
         <CardActions>
-            <Button size="small">Save</Button>
+            <Grid container spacing="1">
+                <Grid item xs={12} >
+                    <Button size="small" onClick={onSubmit}>Save</Button>
+                </Grid>
+                <Grid item xs >
+                    <h1>{message}</h1>
+                </Grid>
+            </Grid>
         </CardActions>
     </React.Fragment>)
 };
